d176c69a3df653e1df47be0b9e9ad7810fb55768
loctran1906 <91265286+loctran1906@users.noreply.github.com>
2022-02-14 18:08:52 +0700

    ud

--- views/client/contact/contact.php
+++ views/client/contact/contact.php
@@ -11,55 +11,61 @@
         </div>
     </div>
 </div>
-<div class="contact flex-center">
-    <div class="container">
+<div class="contact " style="background-image: url('img/banner/bg-contact.jpg');">
+    <div class="container flex-center contact-content">
         <div class="row">
-            <form>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="inputEmail4">Email</label>
-                        <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+            <div class="col-sm-6">
+                <form> 
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="inputEmail4">Email</label>
+                            <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="inputPassword4">Password</label>
+                            <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                        </div>
                     </div>
-                    <div class="form-group col-md-6">
-                        <label for="inputPassword4">Password</label>
-                        <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                    <div class="form-group">
+                        <label for="inputAddress">Address</label>
+                        <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
                     </div>
+                    <div class="form-group">
+                        <label for="phonenumber">Phone number</label>
+                        <input type="number" class="form-control" id="phonenumber" placeholder="">
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-8">
+                            <label for="fullname">Full name</label>
+                            <input type="text" class="form-control" id="fullname">
+                        </div>
+                        <div class="form-group col-md-4">
+                            <label for="inputState">Gender</label>
+                            <select id="inputState" class="form-control">
+                                <option selected>---Choose---</option>
+                                <option>Female</option>
+                                <option>Male</option>
+                                <option>Other...</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" id="gridCheck">
+                            <label class="form-check-label" for="gridCheck">Would you like to become our member?</label>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Sign in</button>
+                </form>
+            </div>
+            <div class="col-sm-6">
+                <div>
+                    <h2>We'd love to hear your feedback</h2>
                 </div>
-                <div class="form-group">
-                    <label for="inputAddress">Address</label>
-                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                <div>
+                    <p>You can rest assured that any information you provide is kept confidential by us. We will contact you soon to hear all from you.</p>
                 </div>
-                <div class="form-group">
-                    <label for="inputAddress2">Address 2</label>
-                    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-                </div>
-                <div class="form-row">
-                    <div class="form-group col-md-6">
-                        <label for="inputCity">City</label>
-                        <input type="text" class="form-control" id="inputCity">
-                    </div>
-                    <div class="form-group col-md-4">
-                        <label for="inputState">State</label>
-                        <select id="inputState" class="form-control">
-                            <option selected>Choose...</option>
-                            <option>...</option>
-                        </select>
-                    </div>
-                    <div class="form-group col-md-2">
-                        <label for="inputZip">Zip</label>
-                        <input type="text" class="form-control" id="inputZip">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" id="gridCheck">
-                        <label class="form-check-label" for="gridCheck">
-                            Check me out
-                        </label>
-                    </div>
-                </div>
-                <button type="submit" class="btn btn-primary">Sign in</button>
-            </form>
+            </div>
         </div>
     </div>
 </div>
