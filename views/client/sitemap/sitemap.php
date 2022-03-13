0fd2a1ca44d8583209b3022f7c0019cfb41f8d3d
loctran1906 <91265286+loctran1906@users.noreply.github.com>
2022-03-13 22:16:30 +0700

    finish

--- views/client/sitemap/sitemap.php
+++ views/client/sitemap/sitemap.php
@@ -1,4 +1,3 @@
-
 <div class="banner">
     <div class="container-fluid">
         <div class="row">
@@ -26,19 +25,7 @@
 
                 <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
                     <div class="card-body">
-                        <ul>
-                            <li>Home</li>
-                            <li>About Us</li>
-                            <li>
-                                <p>Product</p>
-                                <ul>
-                                    <li>Brand 1</li>
-                                    <li>Brand 2</li>
-                                </ul>
-                            </li>
-                            <li>Contact Us</li>
-                            <li>Site map</li>
-                        </ul>
+                        <img src="img/sitemap/sitemap-home.jpg" alt="">
                     </div>
                 </div>
             </div>
@@ -47,28 +34,13 @@
                     <h5 class="mb-0">
                         <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                             <i class="fa" aria-hidden="true"></i>
-                            Collapsible Group Item #2
+                            Admin
                         </button>
                     </h5>
                 </div>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
                     <div class="card-body">
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                    </div>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-header" id="headingThree">
-                    <h5 class="mb-0">
-                        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                            <i class="fa" aria-hidden="true"></i>
-                            Collapsible Group Item #3
-                        </button>
-                    </h5>
-                </div>
-                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-                    <div class="card-body">
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        <img src="img/sitemap/sitemap-admin.jpg" alt="">
                     </div>
                 </div>
             </div>
